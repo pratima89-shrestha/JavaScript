6cbfe63030d570381a2e64838cddcd38184fc67a
pratima89-shrestha <crexthapratima111@gmail.com>
2025-01-14 16:19:16 +0545

    foreach array is added

---
+++ For_each/for_each.js
@@ -0,0 +1,15 @@
+// let a=[1,2,3,4];
+// a.forEach(()=>{
+// console.log(a)   
+// })
+
+
+// let a =[1,2,3,4]
+// a.forEach((array)=>{
+//     console.log(a);
+// })
+
+let a=[2,3,4]
+a.forEach((value)=>{
+    console.log(value *2)
+})
