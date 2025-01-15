4a776ec0d1b83130ca4c6b128b7b2b5ec2915943
pratima89-shrestha <crexthapratima111@gmail.com>
2025-01-15 16:33:49 +0545

    data is called

---
+++ JS-for-react/asyn_await_fetch/callbackHell.js
@@ -0,0 +1,42 @@
+//callback hell 
+//nested callbacks stacked below one another forming a pyramid structure
+//the style of programming becomes difficult to understand and image
+
+//nested if else
+let age=19;
+if(age>=45){
+    if(age>=60){
+        console.log("senior");
+    }
+    else{
+        console.log("middle");
+    }
+}
+else{
+    console.log("child");
+}
+
+
+//nested loop
+//writing for loop under another for loop
+for(let i=0;i<5;i++){
+    let str="";
+    for(let j=0;j<5;j++){
+        str=str+j;
+    }
+        console.log(i,str);
+    }
+
+
+
+ //trying the access the data from the database
+function getData(dataId){
+    setTimeout(()=>{
+        console.log("data",dataId);
+    },2000)
+}
+
+getData(1);
+getData(2);
+getData(3);
+
